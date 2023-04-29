4e3796eaf9e5e66c599844798e9b1a56f8b34054
Vu Duc Tiep <vuductiep@live.com>
2023-04-29 10:22:43 +0900

    add sanity package

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
         <footer className='sticky bottom-5 w-full cursor-pointer'>
           <div className='flex items-center justify-center'>
             <ArrowUpIcon className='h-10 w-10 rounded-full filter 
-              grayscale hover:grayscale-0 cursor-pointer opacity-0 hover:opacity-100'/>
+              grayscale hover:grayscale-0 cursor-pointer opacity-5 hover:opacity-100'/>
           </div>
         </footer>
       </Link>
